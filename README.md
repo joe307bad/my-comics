82c9bc0c4a6f3a61d2e75c91b01b01c723535258
Joe <badaczewskij@live.com>
2017-08-14 22:24:53 -0400

    adding full protractor test for search -> add comic -> open menu -> remove comic

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-## My Comics App
+## ComicTracker
+
+Keep track of all the comics you read, gain points, earn badges, recommend issues/volumes to friends, and follow latest releases.
+ 
+ Built with:
+ 
+ - Ionic 3/Angular 4
+ - ngrx 4
+ - Immutable.js
+ - Karma/Jasmine/Protractor
+ - Moment.js
+ - (Coming soon) PouchDB/CouchDB
+ - (Coming soon) TravisCI
+ 
+ ToDo:
+ 
+ - Finalize Protractor tests
+  -- use PageObjects
+  -- simplify isDisplayed promises
+  -- split up tests into multiple it blocks
+ 
+ - Integrate CouchDB/PouchDB
+ 
+ - Integrate TravisCI
--- e2e/search.e2e-spec.ts
+++ e2e/search.e2e-spec.ts
@@ -6,31 +6,41 @@ describe('search results and crud e2e test', () => {
     browser.get('/#/nav/n4/search');
   });
 
-  it('reach the search page', () => {
-    expect(browser.getTitle()).toEqual('Search Results');
-  });
-
-  it('search button is present', () => {
-    expect(element(by.css('#getSearchResults')).isPresent()).toBeTruthy();
-  });
-
-  it('execute a search', () => {
+  it('execute a search, add a comic, open menu, and remove comic', () => {
 
     'superman'.split('').forEach((c) => element(by.css('#searchQuery .text-input')).sendKeys(c));
     element(by.css('#getSearchResults')).click();
     browser.driver.sleep(5000);
 
     browser.driver.findElements(by.css('#search-results ion-item')).then(function (elems) {
+
       expect(elems.length).toBeGreaterThan(0);
+
+
+      let addButtons = element(by.css('#search-results ion-item button'));
+      let removeButtons = element(by.css('#my-comics ion-item button'));
+
+      addButtons.isDisplayed().then(function (isDisplayed) {
+        if (isDisplayed) {
+          addButtons.click().then(function () {
+            browser.driver.sleep(5000);
+            element(by.css('.open-menu')).click();
+            browser.driver.sleep(5000);
+
+            removeButtons.isDisplayed().then(function (isDisplayed1) {
+              if (isDisplayed1) {
+                removeButtons.click();
+                browser.driver.sleep(5000);
+              } else {
+                throw new Error('Error occurred!');
+              }
+            });
+          });
+        } else {
+          throw new Error('Error occurred!');
+        }
+      });
     });
-
-  });
-
-  it('add a comic to the list of comics', () => {
-    element.all(by.css('#search-results ion-item button')).click();
-  });
-
-  it('check if comic has been added to my comics', () => {
 
   });
 });
